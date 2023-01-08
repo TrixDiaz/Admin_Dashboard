8d7b9983d3b17c17c2bde7dcca1870dae9c0f008
XirTech <john.darlucio022@gmail.com>
2023-01-09 01:51:31 +0800

    add navbar

--- index.php
+++ index.php
@@ -1,8 +1,25 @@
 <?php 
- include './assets/partials/_header.php';
+ include './assets/partials/_head.php';
 ?>
 <title>Home</title>
-
-
-
-<?php include './assets/partials/_footer.php'; ?>
+<div class="container-fluid fixed-top">
+    <div class="container h5">
+       <div class="row my-1 ">
+            <div class="col">
+                <a href="#" class="mx-5 text-decoration-none">Logo</a>
+            </div>
+            <div class="col">
+            <a href="#" class="mx-5 text-decoration-none" data-bs-toggle="modal" data-bs-target="#myModal"> 
+                <i class="bi bi-box-arrow-in-right mx-2"></i>
+                Login
+            </a>
+                
+                <a href="#" class="mx-5 text-decoration-none">Logo</a>
+                <a href="#" class="mx-5 text-decoration-none">Logo</a>
+            </div>
+       </div>
+    </div>
+</div>
+<?php include './assets/partials/_loginModal.php'; ?>
+<?php include './assets/scripts/index.php' ?>
+<?php include './assets/partials/_foot.php'; ?>
