a56bc558c24333b731538ef78f4119aeeaa6f0b1
XirTech <john.darlucio022@gmail.com>
2023-01-15 04:52:38 +0800

    middle nav link

--- assets/partials/_sidebar.php
+++ assets/partials/_sidebar.php
@@ -2,12 +2,13 @@
 <title>Admin | Dashboard</title>
 
 <div class="container-fluid">
-
+   
     <div class="row flex-nowrap">
         <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
-            <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
+          <div class="container mx-3">
+          <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
                 <a href="/" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-                    <span class="fs-5 d-none d-sm-inline">Menu</span>
+                <i class="fs-4 bi-bootstrap"></i><span class="fs-5 d-none d-sm-inline mx-3">Menu</span>
                 </a>
                 <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
                     <li class="nav-item">
@@ -83,4 +84,5 @@
                     </ul>
                 </div>
             </div>
+          </div>
         </div>
