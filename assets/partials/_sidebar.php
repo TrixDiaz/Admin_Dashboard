e7b22138a749d7b1439944eaa3f4738c3a2cca29
XirTech <john.darlucio022@gmail.com>
2023-01-15 11:27:51 +0800

    change icon

--- assets/partials/_sidebar.php
+++ assets/partials/_sidebar.php
@@ -71,7 +71,7 @@
                 <hr>
                 <div class="dropdown pb-4">
                     <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-                        <img src="https://github.com/mdo.png" alt="hugenerd" width="30" height="30" class="rounded-circle">
+                        <img src="https://pbs.twimg.com/media/DW5LJ7LW4AAMC70.png" alt="hugenerd" width="30" height="30" class="rounded-circle">
                         <span class="d-none d-sm-inline mx-1">loser</span>
                     </a>
                     <ul class="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
