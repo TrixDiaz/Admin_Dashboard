4d441a695dfd0488abce4a93b7cd9afbfe05cd56
Trix <john.darlucio022@gmail.com>
2023-01-29 10:19:47 +0800

    fix update profile

--- assets/partials/_profileModal.php
+++ assets/partials/_profileModal.php
@@ -21,20 +21,16 @@
                                <div class="col-auto mb-2">
                                         <div class="row">
                                             <div class="col-4">
-                                                <span>Age</span>
+                                                <span><span><?php echo "<strong>Age</strong>" . "<br>" . $result['birth_date']; ?></span></span>
                                             </div>
                                             <div class="col-4">
-                                                <span><?php echo "Birth Date " . "<br>" . $result['birth_date']; ?></span>
+                                                <span><?php echo "<strong>Birth Date</strong>" . "<br>" . $result['birth_date']; ?></span>
                                             </div>
                                             <div class="col-4">
-                                                <span><?php echo "Contact Number" . "<br>" .  "<strong>+63</strong> " . $result['contact_number']; ?></span>
+                                                <span><?php echo "<strong>Contact Number</strong>" . "<br>" .  "<strong>+63</strong> " . $result['contact_number']; ?></span>
                                             </div>
                                         </div>
-                               </div>
-                               <div class="col-auto my-3">
-                                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
-                               </div>
-                               
+                               </div>                               
                             </div>
                         </div>
                     </div>
@@ -84,64 +80,27 @@
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-         <input type="text" value="<?php echo $result['user_name']; ?>" class="form-control mb-3 mt-3" disabled>
-              <div class="input-group mb-3 mt-3">
-                  <span class="input-group-text">First & last name</span>
-                  <input type="text" aria-label="First name" placeholder="<?php echo $result['user_lastname']; ?>" class="form-control" > 
-                  <input type="text" aria-label="Last name" placeholder="<?php echo $result['user_firstname']; ?>" class="form-control">
+        <div class="mb-3">
+          <label for="username" class="form-label"><strong>Username</strong></label>
+          <input type="text" value="<?php echo $result['user_name']; ?>" class="form-control" id="username" disabled>
+        </div>
+        <div class="mb-3">
+             <label for="fullname" class="form-label"><strong>Full Name</strong></label>
+              <div class="input-group">
+                  <span class="input-group-text" id="fullname"><?php echo $result['user_lastname']; ?> <?php echo $result['user_firstname']; ?> <?php echo $result['user_middlename']; ?></span>
+                  <input type="text" aria-label="First name" placeholder="First Name" class="form-control" > 
+                  <input type="text" aria-label="Last name" placeholder="Last Name" class="form-control">
+                  <input type="text" aria-label="Middle name" placeholder="Middle Name" class="form-control">
             </div>
-            <div class="input-group mt-3 mb-3">
-              <input type="text" class="form-control" placeholder="YEAR" required>
-              <select class="form-select text-uppercase" name="month" required>
-                  <li><option selected disabled>Month</option></option></li>
-                  <li><option value="01">January</option></li>
-                  <li><option value="02">February</option></li>
-                  <li><option value="03">March</option></li>
-                  <li><option value="04">April</option></li>
-                  <li><option value="05">May</option></li>
-                  <li><option value="06">June</option></li>
-                  <li><option value="07">July</option></li>
-                  <li><option value="08">August</option></li>
-                  <li><option value="09">September</option></li>
-                  <li><option value="10">October</option></li>
-                  <li><option value="11">November</option></li>
-                  <li><option value="12">December</option></li>
-              </select>
-              <select class="form-select text-uppercase" name="day" required>
-                  <li><option selected disabled>Day</option></option></li>
-                  <li><option value="01">01</option></li>
-                  <li><option value="02">02</option></li>
-                  <li><option value="03">03</option></li>
-                  <li><option value="04">04</option></li>
-                  <li><option value="05">05</option></li>
-                  <li><option value="06">06</option></li>
-                  <li><option value="07">07</option></li>
-                  <li><option value="08">08</option></li>
-                  <li><option value="09">09</option></li>
-                  <li><option value="10">10</option></li>
-                  <li><option value="11">11</option></li>
-                  <li><option value="12">12</option></li>
-                  <li><option value="13">13</option></li>
-                  <li><option value="14">14</option></li>
-                  <li><option value="15">15</option></li>
-                  <li><option value="16">16</option></li>
-                  <li><option value="17">17</option></li>
-                  <li><option value="18">18</option></li>
-                  <li><option value="19">19</option></li>
-                  <li><option value="20">20</option></li>
-                  <li><option value="21">21</option></li>
-                  <li><option value="22">22</option></li>
-                  <li><option value="23">23</option></li>
-                  <li><option value="24">24</option></li>
-                  <li><option value="25">25</option></li>
-                  <li><option value="26">26</option></li>
-                  <li><option value="27">27</option></li>
-                  <li><option value="28">28</option></li>
-                  <li><option value="29">29</option></li>
-                  <li><option value="30">30</option></li>
-                  <li><option value="31">31</option></li>
-              </select>
-                <input type="text" class="form-control" placeholder="+63 <?php echo $result['contact_number']; ?>">
+        </div>
+            <div class="mb-3">
+            <label for="birthdate" class="form-label"><strong>Birthdate & Contact Number</strong></label>
+            <div class="input-group">
+             <span class="input-group-text" id="birthdate"><?php echo $result['birth_date']; ?></span>
+              <input type="date" class="form-control" id="contact">
+              <span class="input-group-text">+63 <?php echo $result['contact_number']; ?></span>
+              <input type="text" class="form-control" placeholder="Contact Number">
+            </div>
             </div>
       </div>
       <div class="modal-footer">
